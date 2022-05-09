fa3ae97ccdb6cd9a59c7977b7da822854d954d1f
georgianacrisan99@gmail.com <georgianacrisan99@gmail.com>
2022-05-09 14:17:15 +0300

    Initial file structure

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainPage(props) {
+    const { title } = props;
+    console.log(title);
+    return (
+        <div id="MainPage">
+
+        </div>
+    );
+
+}
+
+
+export default MainPage;
